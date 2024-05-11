09fcee91c344f57cd98348885c1e48e4448e8a52
mohammed <mohammedbakkali807@gmail.com>
2024-05-11 14:50:08 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
+import Pricing from "./components/pricing/Pricing";
+import testimonials from "./components/testimonials/Testimonials";
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./sidebat.css"
+
+const Sidebar = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  );
+}
+
+export default Sidebar;
